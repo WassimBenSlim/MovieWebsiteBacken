04a90da33ae00ead7c1ccd8f566c7c6655f12ea8
WassimBenSlim <wisskjkj@gmail.com>
2024-06-19 18:45:34 +0100

    Initial commit

---
+++ movies/src/main/java/dev/wassim/movies/Repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.wassim.movies.Repositories;
+
+import dev.wassim.movies.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
